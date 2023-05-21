f96e30d6a0f88a5bfcef77f77e35d10985c38944
Luke Hunter-Salmons <lukehsalmons@gmail.com>
2023-05-21 12:31:15 +0100

    Commit App

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1', //localhost
+      user : 'lukesalmons', //add your user name for the database here
+      password : '', //add your correct password in here
+      database : 'smart-brain' //add your database name you created here
+    }
+});
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {res.send('success')})
+
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
+
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
+
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
+
+app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+
+app.listen(3001, () => {
+    console.log('App is running');
+})
